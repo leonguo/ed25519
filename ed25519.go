2d50f46f6540b75d80b4790d59aecdf2a580514d
guoxingjun <hotman8168@gmail.com>
2018-07-08 14:52:49 +0800

    gen key by string

--- ed25519.go
+++ ed25519.go
@@ -15,6 +15,7 @@ import (
 	"io"
 
 	"github.com/agl/ed25519/edwards25519"
+	"strings"
 )
 
 const (
@@ -28,6 +29,33 @@ func GenerateKey(rand io.Reader) (publicKey *[PublicKeySize]byte, privateKey *[P
 	privateKey = new([64]byte)
 	publicKey = new([32]byte)
 	_, err = io.ReadFull(rand, privateKey[:32])
+	if err != nil {
+		return nil, nil, err
+	}
+
+	h := sha512.New()
+	h.Write(privateKey[:32])
+	digest := h.Sum(nil)
+
+	digest[0] &= 248
+	digest[31] &= 127
+	digest[31] |= 64
+
+	var A edwards25519.ExtendedGroupElement
+	var hBytes [32]byte
+	copy(hBytes[:], digest)
+	edwards25519.GeScalarMultBase(&A, &hBytes)
+	A.ToBytes(publicKey)
+
+	copy(privateKey[32:], publicKey[:])
+	return
+}
+
+// GenerateKey generates a public/private key pair using  32 bit md5 string
+func GenerateKeyBystring(str string) (publicKey *[PublicKeySize]byte, privateKey *[PrivateKeySize]byte, err error) {
+	privateKey = new([64]byte)
+	publicKey = new([32]byte)
+	_, err = io.ReadFull(strings.NewReader(str), privateKey[:32])
 	if err != nil {
 		return nil, nil, err
 	}
